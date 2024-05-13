98582837fc089935e71776a75f26be89972a2811
LucyManalang <lucemanalang@gmail.com>
2024-05-12 22:32:38 -0700

    day 2 done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,60 +1,123 @@
 import Image from 'next/image';
-import { MdLightMode, MdDarkMode } from 'react-icons/md';
+import { useLocalStorage } from 'react-use';
+import { AiOutlineMoon, AiOutlineSun } from 'react-icons/ai';
 import { PiLinkedinLogo, PiGithubLogoLight } from 'react-icons/pi';
 import Link from 'next/link';
 import React from 'react';
-
-let lightMode = false;
 
 const images = {
   portrait: '/portrait.jpeg',
   logo: '/macalesterlogo.png',
 };
 
-function Navigation() {
+const socials = [
+  {
+    logo: <PiLinkedinLogo />,
+    link: 'https://linkedin.com/in/lucy-manalang-5905582b4',
+    id: 1,
+  },
+  {
+    logo: <PiGithubLogoLight />,
+    link: 'https://github.com/LucyManalang',
+    id: 2,
+  },
+];
+
+const socialItems = socials.map((social) => (
+  <li key={social.id} className="text-text text-6xl hover:text-secondary">
+    <Link href={social.link} target="_blank">
+      {social.logo}
+    </Link>
+  </li>
+));
+
+const socialList = <ul className="inline-flex">{socialItems}</ul>;
+
+const Navigation = () => {
+  const [dark, toggleDark, remove] = useLocalStorage('dark', true);
+
   return (
-    <div id="nav">
-      <ul id="nav-bar">
-        <Link className="nav-item" href="/">
+    <nav className="fixed top-0 w-screen border-b-2 border-secondary bg-base-200 drop-shadow-lg">
+      <div className="flex flex-row flex-nowrap justify-between mx-3 my-1">
+        <Link href="/">
           <h1>Lucy Manalang</h1>
         </Link>
-        <li className="">
-          <Link href="">
-            <p>
-              <button>{lightMode ? <MdDarkMode /> : <MdLightMode />}</button>
-            </p>
-          </Link>
-        </li>
-        <li className="">
-          <Link href="">
-            <p>About</p>
-          </Link>
-        </li>
-        <li className="">
-          <Link href="">
-            <p>Projects</p>
-          </Link>
-        </li>
-        <li className="">
-          <Link href="">
-            <p>Content</p>
-          </Link>
-        </li>
-      </ul>
-    </div>
+        <div className="flex flex-row items-center gap-5">
+          <p className="w-8 flex align-middle justify-center hover:text-secondary">
+            <label className="swap swap-rotate items-center justify-center">
+              <input
+                type="checkbox"
+                className="theme-controller"
+                value={dark ? 'mydark' : 'mylight'}
+                onClick={() => toggleDark(!dark)}
+              />
+              <AiOutlineSun className="text-xl fixed swap-off" />
+              <AiOutlineMoon className="text-xl swap-on" />
+            </label>
+          </p>
+          <ul className="flex flex-row items-center mr-4 gap-10">
+            <li>
+              <Link
+                onClick={(evt) => {
+                  evt.preventDefault();
+                  document
+                    .querySelector('#about-me')
+                    ?.scrollIntoView({ behavior: 'smooth' });
+                }}
+                href="#about-me"
+              >
+                <p className="h-max flex align-middle justify-center hover:text-secondary">
+                  About
+                </p>
+              </Link>
+            </li>
+            <li>
+              <Link
+                onClick={(evt) => {
+                  evt.preventDefault();
+                  document
+                    .querySelector('#projects')
+                    ?.scrollIntoView({ behavior: 'smooth' });
+                }}
+                href="#projects"
+              >
+                <p className="flex align-middle justify-center hover:text-secondary">
+                  Projects
+                </p>
+              </Link>
+            </li>
+            <li>
+              <Link
+                onClick={(evt) => {
+                  evt.preventDefault();
+                  document
+                    .querySelector('#contact')
+                    ?.scrollIntoView({ behavior: 'smooth' });
+                }}
+                href="#contact"
+              >
+                <p className="flex align-middle justify-center hover:text-secondary">
+                  Contact
+                </p>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
-}
+};
 
-export default function Home() {
+const App = () => {
   return (
     <>
-      <main className="mx-40 my-40 flex flex-col gap-y-96">
-        <div className="flex justify-between">
-          <div>
+      <main className="flex flex-col">
+        <div id="hero" className="px-40 pt-64 h-screen flex flex-wrap-reverse">
+          <div className="max-w-sm">
             <h1>Hi, I{"'"}m Lucy!</h1>
             <div>
-              <p className="text-wrap">
-                I{"'"}m a Computer Science and Studio Art student at
+              <p className="text-nowrap">
+                I{"'"}m a Computer Science and Studio Art student <br /> at
                 <Link
                   className="hover:text-secondary"
                   href="https://www.macalester.edu/"
@@ -63,19 +126,21 @@ export default function Home() {
                   <img className="w-5 inline" src={images.logo} />
                   Macalester College{' '}
                 </Link>
-                based in Oakland, CA.
+                and I am based in <br />
+                Oakland, CA.
               </p>
             </div>
+            <div className="mx-5 my-5">{socialList}</div>
           </div>
           <div>
             <img
-              className="max-w-72"
+              className="max-w-72 rounded-[15px]"
               src={images.portrait}
               alt="Picture of me"
             />
           </div>
         </div>
-        <div className="">
+        <div id="about-me" className="px-40 py-28 bg-base-200">
           <h1>About Me:</h1> <br />
           <p>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore,
@@ -84,7 +149,7 @@ export default function Home() {
             possimus deserunt eaque vel.
           </p>
         </div>
-        <div className="section">
+        <div id="projects" className="px-40 py-28">
           <h1>Projects:</h1> <br />
           <p>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore,
@@ -93,16 +158,15 @@ export default function Home() {
             possimus deserunt eaque vel.
           </p>
         </div>
-        <div className="contact">
-          <div className="section">
-            <h1>Contact Me:</h1>
-            <PiLinkedinLogo className="text-text text-6xl" />
-            <PiGithubLogoLight className="text-text text-6xl" />
-            <br />
-          </div>
+        <div id="contact" className="px-40 py-28 bg-base-200">
+          <h1>Contact Me:</h1>
+          {socialList}
+          <br />
         </div>
       </main>
       <Navigation />
     </>
   );
-}
+};
+
+export default App;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,24 +7,27 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    colors: {
-      text: '#e3e3e3',
-      background: '#1a1a1a',
-      primary: '#393e97',
-      secondary: '#5387CE',
-      accent: '#54AE73',
-    },
+    colors: {},
   },
   plugins: [require('daisyui')],
   daisyui: {
     themes: [
       {
-        mytheme: {
-          text: '#e3e3e3',
-          background: '#1a1a1a',
-          primary: '#393e97',
-          secondary: '#5387CE',
-          accent: '#54AE73',
+        mydark: {
+          'primary': '#393e97',
+          'secondary': '#5387CE',
+          'accent': '#54AE73',
+          'neutral': '#e3e3e3',
+          'base-100': '#1a1a1a',
+          'base-200': '151515',
+        },
+        mylight: {
+          'primary': '#676cc5',
+          'secondary': '#5387CE',
+          'accent': '#50aa6f',
+          'neutral': '#1c1c1c',
+          'base-100': '#e6e6e6',
+          'base-200': '#D4D4D4',
         },
       },
     ],
